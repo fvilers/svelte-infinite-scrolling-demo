33645d354dba14f42762869c4f0d723ce2bc52e7
Fabian Vilers <fabian.vilers@dev-one.com>
2023-08-21 11:43:25 +0200

    Display items

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,16 @@
+<script lang="ts">
+  type Item = {
+    id: number;
+    text: string;
+  };
+
+  const items: Item[] = [...Array(1000).keys()].map((id) => ({ id, text: "Value " + id }));
+</script>
+
 <h1>Infinite Scroll Demo</h1>
+
+<ul>
+  {#each items as item (item.id)}
+    <li>{item.text}</li>
+  {/each}
+</ul>
